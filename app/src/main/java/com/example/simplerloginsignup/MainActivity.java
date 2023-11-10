fd376667a7642f19ff28148e1ea76983f1a24c4c
Kuroko <133122584+RenzSarucam@users.noreply.github.com>
2023-11-11 00:11:49 +0800

    SimplerLoginsignup

--- app/src/main/java/com/example/simplerloginsignup/MainActivity.java
+++ app/src/main/java/com/example/simplerloginsignup/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.simplerloginsignup;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Load home_page.xml
+
+        TextView home = findViewById(R.id.welcome); // Fix the TextView ID
+
+        Button button1, button2, button3;
+        button1 = findViewById(R.id.red); // Fix the Button IDs
+        button2 = findViewById(R.id.blue);
+        button3 = findViewById(R.id.green);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                home.setTextColor(Color.RED);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                home.setTextColor(Color.BLUE); // Change to BLUE
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                home.setTextColor(Color.GREEN);
+            }
+        });
     }
 }
